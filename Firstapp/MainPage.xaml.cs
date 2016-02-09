dc892be03eea685de88dc3ecf104ff61f541fc32
Saamari <sampsa_kautto@hotmail.com>
2016-02-09 10:22:23 +0200

    mo

---
+++ Firstapp/MainPage.xaml.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+// The Blank Page item template is documented at http://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
+
+namespace Firstapp
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class MainPage : Page
+    {
+        public MainPage()
+        {
+            this.InitializeComponent();
+        }
+
+        private void infoButton_Click(object sender, RoutedEventArgs e)
+        {
+            //Button painettu = 8Button sender;
+            infoTextBlock.Text = " Info MY first UWP app!"; 
+        }
+
+        private void secondButton_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
